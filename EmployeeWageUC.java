8126d39eba368069bd670a0760e8abba9cf8bec1
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-28 20:46:48 +0530

    Added - Added parttime wage and employee

--- EmployeeWageUC.java
+++ EmployeeWageUC.java
@@ -3,14 +3,18 @@ public class EmployeeWageUC{
 	public static void main(String[] args) {
 
 	int EMP_WAGE_PER_HOUR = 20;
-        int IS_FULL_TIME = 1;
+       	int IS_FULL_TIME = 1;
+	int IS_PART_TIME = 2;
 	int empHours = 0;
 	int empWage = 0;
-        double empChecker = (int)Math.floor(Math.random() * 10) % 2;
+        double empChecker = (int)Math.floor(Math.random() * 10) % 3;
 		
         if(empChecker == IS_FULL_TIME) {
             	empHours = 8;
         }
+	else if(empChecker == IS_PART_TIME){
+		empHours = 4;
+	}
         else {
 		empHours = 0;
         }
