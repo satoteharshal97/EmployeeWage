4f135b0c283d18a474b9404f39d4c05a7e7220bb
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-28 20:51:08 +0530

    Added - Solved using SwitchCase

--- EmployeeWageUC.java
+++ EmployeeWageUC.java
@@ -1,22 +1,21 @@
 public class EmployeeWageUC{
+	public static final int EMP_WAGE_PER_HOUR = 20;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
 	
-	public static void main(String[] args) {
-
-	int EMP_WAGE_PER_HOUR = 20;
-       	int IS_FULL_TIME = 1;
-	int IS_PART_TIME = 2;
+	public static void main(String[] args) {	
 	int empHours = 0;
 	int empWage = 0;
-        double empChecker = (int)Math.floor(Math.random() * 10) % 3;
-		
-        if(empChecker == IS_FULL_TIME) {
-            	empHours = 8;
-        }
-	else if(empChecker == IS_PART_TIME){
-		empHours = 4;
-	}
-        else {
-		empHours = 0;
+        int empChecker = (int)Math.floor(Math.random() * 10) % 3;	
+        switch(empChecker){
+			case IS_FULL_TIME:
+				empHours = 8;
+				break;
+			case IS_PART_TIME:
+				empHours = 4;
+				break;
+			default:
+				empHours = 0;
         }
 	empWage = empHours * EMP_WAGE_PER_HOUR;
 	System.out.println("Employee Wage: " + empWage);
