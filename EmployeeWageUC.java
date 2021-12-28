35e7dbfe25571374027ceeded00a7a702c0bc3a1
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-28 20:32:15 +0530

    Added - Check employee absent or present

--- EmployeeWageUC.java
+++ EmployeeWageUC.java
@@ -1,7 +1,14 @@
 public class EmployeeWageUC{
 	
 	public static void main(String[] args) {
-        	
-		System.out.println("Welcome to EmployeeWage");
+        int IS_FULL_TIME = 1;
+        double empChecker = (int)Math.floor(Math.random() * 10) % 2;
+        if(empChecker == IS_FULL_TIME) {
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
+
     }
 }
