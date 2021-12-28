a9ee6ebc29b4229dcac5cd48e623fc4b46658c12
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-28 20:42:20 +0530

    Added - Calculated Daily Employee Wage

--- EmployeeWageUC.java
+++ EmployeeWageUC.java
@@ -1,14 +1,20 @@
 public class EmployeeWageUC{
 	
 	public static void main(String[] args) {
+
+	int EMP_WAGE_PER_HOUR = 20;
         int IS_FULL_TIME = 1;
+	int empHours = 0;
+	int empWage = 0;
         double empChecker = (int)Math.floor(Math.random() * 10) % 2;
+		
         if(empChecker == IS_FULL_TIME) {
-            System.out.println("Employee is Present");
+            	empHours = 8;
         }
         else {
-            System.out.println("Employee is Absent");
+		empHours = 0;
         }
-
+	empWage = empHours * EMP_WAGE_PER_HOUR;
+	System.out.println("Employee Wage: " + empWage);
     }
 }
